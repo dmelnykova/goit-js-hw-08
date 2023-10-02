850daab3137a29fa0e8ca20eb7b4b7b38d953767
dariameln1 <dariamelnykova1@gmail.com>
2023-10-02 19:15:30 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LS_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(addKey, 1000));
+
+function addKey(event) {
+    const currentTime = event.seconds;
+    localStorage.setItem(LS_KEY, currentTime);
+}
+
+const savedTime = parseFloat(localStorage.getItem(LS_KEY));
+
+if (!isNaN(savedTime)) {
+    player.setCurrentTime(savedTime);
+} else {
+    console.log('No saved time available.');
+}
