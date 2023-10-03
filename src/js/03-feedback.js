62840ad4439777cc6a63f5a1bef538bdf8e288ec
dariameln1 <dariamelnykova1@gmail.com>
2023-10-03 14:52:02 +0200

    task-03 updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,6 @@ import throttle from 'lodash.throttle';
 const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = document.querySelector('input[name="email"]');
 const messageTextarea = document.querySelector('textarea[name="message"]');
-
 const LC_KEY = 'feedback-form-state';
 
 feedbackForm.addEventListener('input', throttle(handleSaveToLS, 500));
@@ -17,29 +16,29 @@ function handleSaveToLS() {
   localStorage.setItem(LC_KEY, JSON.stringify(formData));
 }
 
-
 function loadFormState() {
   const savedFormData = localStorage.getItem(LC_KEY);
   if (savedFormData) {
     const formData = JSON.parse(savedFormData);
     emailInput.value = formData.email || '';
     messageTextarea.value = formData.message || '';
-    };
-};
+  }
+}
 
 document.addEventListener('DOMContentLoaded', loadFormState);
-
-emailInput.addEventListener('input', handleSaveToLS);
-messageTextarea.addEventListener('input', handleSaveToLS);
-
-feedbackForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
 
-  if (emailInput.value === '' || messageTextarea.value === '') {
+  const formDataObject = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  if (formDataObject.email === '' || formDataObject.message === '') {
     const message = 'Please fill all fields!';
     alert(message);
+    return; 
   }
 
   localStorage.removeItem(LC_KEY);
@@ -47,9 +46,9 @@ function handleSubmit(event) {
   emailInput.value = '';
   messageTextarea.value = '';
 
-  const formDataObject = {
-    email: emailInput.value,
-    message: messageTextarea.value,
-  };
   console.log(formDataObject);
-};
+}
+
+feedbackForm.addEventListener('submit', handleSubmit);
+emailInput.addEventListener('input', handleSaveToLS);
+messageTextarea.addEventListener('input', handleSaveToLS);
